078e1baf9571c214956f43a45936d9d61a1c9c37
Oskar Boer <oskarboer@gmail.com>
2023-12-26 19:11:18 +0100

    1155 fixed basic errors

--- problem1155/src/main.rs
+++ problem1155/src/main.rs
@@ -1,14 +1,11 @@
 impl Solution {
     pub fn num_rolls_to_target(n: i32, k: i32, target: i32) -> i32 {
         const MOD: usize = 1_000_000_007;
-        let (n, k, t) = n as usize, k as usize, target as usize;
+        let (n, k, t) = (n as usize, k as usize, target as usize);
 
-        (0..t).iter().fold(0, |a, i| {
-            
+        (0..t).fold(0, |a, i| {
+            0
         })
-
-
-        
     }
 }
 
