4bad6a2f66c9ce352e24032b4d1e2972cfddd1f3
Oskar Boer <oskarboer@gmail.com>
2023-12-26 19:09:20 +0100

    1155 started

---
+++ problem1155/src/main.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn num_rolls_to_target(n: i32, k: i32, target: i32) -> i32 {
+        const MOD: usize = 1_000_000_007;
+        let (n, k, t) = n as usize, k as usize, target as usize;
+
+        (0..t).iter().fold(0, |a, i| {
+            
+        })
+
+
+        
+    }
+}
+
+struct Solution {}
+fn main() {
+    println!("Hello, world!");
+}
