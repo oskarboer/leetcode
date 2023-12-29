e25e46fbc801f7d7081b8ca306dca85e10aab116
Oskar Boer <oskarboer@gmail.com>
2023-12-29 14:17:58 +0100

    1335 partially done yet some bugs are present

---
+++ problem1335/src/main.rs
@@ -0,0 +1,115 @@
+impl Solution {
+    pub fn min_difficulty(job_difficulty: Vec<i32>, d: i32) -> i32 {
+        let d = d as usize;
+        let n = job_difficulty.len();
+        if n < d {
+            return -1;
+        }
+
+        let mut dp = vec![vec![-1; n]; d];
+
+        let mut max = job_difficulty[0];
+        for i in 0..n {
+            if max < job_difficulty[i] {
+                max = job_difficulty[i];
+            }
+            dp[0][i] = max;
+        }
+
+        let mut sum = 0;
+        for i in 0..(n.min(d)) {
+            sum += job_difficulty[i];
+            dp[i][i] = sum;
+        }
+
+
+
+        for day in 1..d {
+            for task in (day + 1)..n {
+                // search for range in what to look for min:
+                let mut prev_big = task - 1;
+                for i in (0..(task-1)).rev() {
+                    if job_difficulty[task] >= job_difficulty[i] {
+                        prev_big = i;
+                    } else {
+                        break;
+                    }
+                }
+                let min_day_before = dp[day - 1][(prev_big.max(day - 1))..=(task-1)].iter().fold(i32::MAX - 1000000, |a, e| a.min(*e)) + job_difficulty[task];
+
+                // // search for big in the same day
+                // let mut prev_larger = task - 1;
+                // for i in (0..(task-1)).rev() {
+                //     if job_difficulty[task] <= job_difficulty[i] {
+                //         prev_larger = i;
+                //     } else {
+                //         break;
+                //     }
+                // }
+
+
+                let mut min_same_day = i32::MAX;
+                if job_difficulty[task - 1] >= job_difficulty[task] {
+                    // min_same_day = dp[day][(prev_larger.max(day))..(task-1)].iter().fold(i32::MAX, |a, e| a.min(*e));
+                    min_same_day = dp[day][task - 1];
+                }
+
+                // println!("day: {day}, task: {task}, min same {min_same_day}, min before {min_day_before}");
+
+                dp[day][task] = min_day_before.min(min_same_day);
+            }
+        }
+
+        for i in &dp {
+            println!("{:?}", i);
+        }
+
+        dp[d-1][n-1]
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        assert_eq!(Solution::min_difficulty(vec![1, 3, 5, 2, 3, 1, 8], 2), 9);
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(Solution::min_difficulty(vec![6,5,4,3,2,1], 2), 7);
+    }
+
+    #[test]
+    fn test3() {
+        assert_eq!(Solution::min_difficulty(vec![6,5,4], 4), -1);
+    }
+
+
+    #[test]
+    fn test4() {
+        assert_eq!(Solution::min_difficulty(vec![1, 3, 5, 2, 3, 1, 8], 3), 12);
+    }
+
+    #[test]
+    fn test5() {
+        assert_eq!(Solution::min_difficulty(vec![10, 3, 5, 1, 5, 3, 8], 3), 19);
+    }
+
+    #[test]
+    fn test6() {
+        assert_eq!(Solution::min_difficulty(vec![186, 398, 479, 206, 885, 423, 805, 112, 925, 656, 16, 932, 740, 292, 671, 360], 4), 1803);
+    }
+
+    #[test]
+    fn test7() {
+        assert_eq!(Solution::min_difficulty(vec![3, 7, 9, 4, 14, 8, 13, 2, 15, 10, 1, 16, 12, 5, 11, 6], 4), 32);
+    }
+}
+
+struct Solution {}
+fn main() {
+    println!("Hello, world!");
+}
